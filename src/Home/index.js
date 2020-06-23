c5b66709820e6c1d44d5dc741116b93266ee1031
MrWilllian <willian.dev10@gmail.com>
2020-06-23 09:45:55 -0300

    refactor: add index home

---
+++ src/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper, Title } from './styles';
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Title>
+        Home
+      </Title>
+    </Wrapper>
+  );
+}
